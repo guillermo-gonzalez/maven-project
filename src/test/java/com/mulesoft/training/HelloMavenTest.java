d8eed6b06dd0e7aecf14850d2b8335977f5794c0
guillermo_gonzalez@gmx.com <guillermo_gonzalez@gmx.com>
2018-04-24 11:37:52 -0400

    added object2json

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,13 +1,22 @@
 package com.mulesoft.training;
 
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
+	@Rule
+	public DynamicPort myPort = new DynamicPort("http.port");
+	
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven!\n");
+        System.out.println("\n\n***** mavenFlowReturnsHelloMavenhttp.port: " + myPort.getNumber() + " *****\n");
     }
     
     @Override
@@ -15,4 +24,11 @@ public class HelloMavenTest extends FunctionalTestCase {
         return "maven-project.xml";
     }
 
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      System.out.println("\n\n***** retrieveFlightsAddsAppropriateHeader.port: " + myPort.getNumber() + " *****\n");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+    }  
 }
